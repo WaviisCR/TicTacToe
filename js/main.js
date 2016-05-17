18b120bbb9b9252aeaad4320ec92203eeba42796
WaviisCR <m.cardenas.vr@gmail.com>
2016-05-17 18:23:25 -0500

    tic tac toe con arreglos OMFG!

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 $(document).ready(function() /*es para cargar primero el html*/ {
-        var jugada = 0;
-        /*seleccionas el elemento sobre el que vas a trabajar ->*/
-        $(".box").click(function() {
-                jugada++; /*contador, suma de 1 en 1*/
-                /*Esta condicion es para las jugadas: para qu empiece en x, y las cajas se desactiven con un click*/
-                if (jugada % 2 === 0) {
-                    /*(this)el elemento sobre el estoy trabajando, en este caso .box / .html es lo que quiero cambiar / (O) por lo que cambiara*/
-                    $(this).html("O")
-                    $(this).css({
-                        "background": "black",
-                        "color": "silver"
-                    }); /*el css va entre comillas y llaves*/
-                    $(this).prop("disabled", true) /*deshabilitar el boton con .prop*/
-                } else {
-                    $(this).html("X")
-                    $(this).css({
-                        "background": "silver",
-                        "color": "black"
-                    });
-                    $(this).prop("disabled", true)
-                }
-//Asignar el contenido de cada box a una variable para luego poer compararlos entre ellos y poder determninar a un ganador.*/
-    
-        var b0 = $("#0").text(); 
+    var jugada = 0;
+    var arreglo = [
+        ["0", "1", "2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"]
+    ]
+    /*seleccionas el elemento sobre el que vas a trabajar ->*/
+    $(".box").click(function() {
+        jugada++; /*contador, suma de 1 en 1*/
+        /*Esta condicion es para las jugadas: para qu empiece en x, y las cajas se desactiven con un click*/
+        if (jugada % 2 === 0) {
+            /*(this)el elemento sobre el estoy trabajando, en este caso .box / .html es lo que quiero cambiar / (O) por lo que cambiara*/
+            $(this).html("O")
+            $(this).css({
+                "background": "black",
+                "color": "silver"
+            }); /*el css va entre comillas y llaves*/
+            $(this).prop("disabled", true) /*deshabilitar el boton con .prop*/
+        } else {
+            $(this).html("X")
+            $(this).css({
+                "background": "silver",
+                "color": "black"
+            });
+            $(this).prop("disabled", true)
+        }
+
+        //Asignar el contenido de cada box a una variable para luego poer compararlos entre ellos y poder determninar a un ganador.*/
+        /* var b0 = $("#0").text(); 
         var b1 = $("#1").text();
         var b2 = $("#2").text();
         var b3 = $("#3").text();
@@ -108,11 +113,50 @@ $(document).ready(function() /*es para cargar primero el html*/ {
         
     });
 
-});  //siempre termina
+});  //siempre termina en ;*/
+
+        /*COMPOSICION DE ARREGLO*/
+
+            /*este es un arreglo de arreglitos*/
+
+        /*ASIGNACION DE VALORES*/
+        arreglo[0][0] = $("#0").text();
+        arreglo[0][1] = $("#1").text();
+        arreglo[0][2] = $("#2").text();
+
+        arreglo[1][0] = $("#3").text();
+        arreglo[1][1] = $("#4").text();
+        arreglo[1][2] = $("#5").text();
+
+        arreglo[2][0] = $("#6").text();
+        arreglo[2][1] = $("#7").text();
+        arreglo[2][2] = $("#8").text();
+
+        /*CICLO FOR PARA ARREGLO: trabajar con indices*/
+        for (var i = 0; i < 3; i++) {
+            if (arreglo[i][0] == arreglo[i][1] && arreglo[i][1] == arreglo[i][2] && arreglo[i][0] != "*") {
+                alert("Gano " + arreglo[i][0] + "!");
+            }
+        }
+
+        for (var i = 0; i < 3; i++) {
+            if (arreglo[0][i] == arreglo[1][i] && arreglo[1][i] == arreglo[2][i] && arreglo[0][i] != "*") {
+                alert("Gano " + arreglo[0][i] + "!");
+            }
+        }
+        if (arreglo[0][0] == arreglo[1][1] && arreglo[0][0] == arreglo[2][2] && arreglo[0][0] != "*") {
+            alert("Gano " + arreglo[0][0] + "!");
+        }
+        if (arreglo[0][2] == arreglo[1][1] && arreglo[0][2] == arreglo[2][0] && arreglo[0][2] != "*") {
+            alert("Gano " + arreglo[0][2] + "!");
+        }
+    });
+
+}); //siempre termina en ;*/
 
 //funcion para recargar(boton)
 $(document).ready(function() {
-     $("#reload").click(function() {
-          location.reload();/*recargo la pagina en el archivo en estoy*/
-     });
+    $("#reload").click(function() {
+        location.reload(); /*recargo la pagina el archivo en el que estoy*/
+    });
 });
